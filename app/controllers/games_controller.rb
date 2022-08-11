f0f318185b9e0245a31a162602482cfc53803191
Danny Iachini <danny.iachini@gmail.com>
2022-08-11 13:34:52 -0400

    Order by created DESC to show newest on top

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,6 @@
 class GamesController < ApplicationController
   # GET /games
   def index
-    @messages = Message.where(created_at: 10.minutes.ago..)
+    @messages = Message.where(created_at: 10.minutes.ago..).order(created_at: :desc)
   end
 end
